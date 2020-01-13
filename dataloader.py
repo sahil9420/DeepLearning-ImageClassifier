772811e1935accb1d406b7d04f78653510b26dc6
Sahil Pavaskar <39223554+sahil9420@users.noreply.github.com>
2020-01-13 22:12:10 +0530

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data/", one_hot = True)
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
+
+n_classes = 10
+batch_size = 100
+
+
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
